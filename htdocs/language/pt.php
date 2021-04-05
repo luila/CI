b6f64ddc92a1acecc08cd0542d3815e2d4932075
Luíla Moraes de Oliveira <luilamoliveira@gmail.com>
2021-04-04 22:56:55 -0300

    Add files via upload

--- htdocs/language/pt.php
+++ htdocs/language/pt.php
@@ -16,6 +16,9 @@
             // Luíla
             //"questionnaire" => "Questionário",
             "questionnaire" => "Questionário Sociodemográfico",
+            "susLibras" => "SUS Libras",
+            "susPortugues" => "SUS Português",
+
 
             //"events" => "Eventos",
             "events" => "Agenda de Solicitações",
@@ -35,6 +38,7 @@
             "sTime" => "Hora Início",
             "eTime" => "Hora Fim",
             "confirm" => "Confirmar",
+            "save" => "Salvar",
             "cancel" => "Cancelar",
             "back" => "Voltar",
 
@@ -44,7 +48,7 @@
             "register" => "Cadastrar Eventos e Usuários",
 
             //"report" => "Relatório",
-            "report" => "Relatórios",
+            "report" => "Relatório",
 
             "description" => "Descrição",
             "chooseInterpreter" => "Sugira até três intérpretes para participar deste evento.",
@@ -59,23 +63,43 @@
 
             "registerEvent" => "Solicitar intérprete para evento",
             "registerEventLabel" => "Clique no botão abaixo para solicitar intérprete para evento.",
-            "registerAge" => "Clique abaixo e selecione sua idade, e depois clique em salvar.",
-            "registerCity" => "Digite abaixo a cidade onde você mora, e depois clique em salvar.",
-            "registerState" => "Digite abaixo o estado onde você mora, e depois clique em salvar.",
-            "registerSex" => "Clique abaixo e selecione o seu sexo, e depois clique em salvar.",
-            "registerWork" => "Você está trabalhando atualmente?",
-            "registerRent" => "Qual seu salário mensal?",
-            "registerScholarity" => "Qual o seu nível de escolaridade?",
-            "registerFluency" => "Você é fluente em Libras?",
-            "registerLibras" => "Desque que idade você é sinalizante da Libras?",
-            "registerDeafness" => "Como você se identifica?",
-            "registerParents" => "Você cresceu em uma casa com pais ou responsáveis sinalizantes da Libras?",
-            "registerParentsDeafness" => "Seus pais ou responsáveis são surdos?",
+            "registerAge" => "Qual a sua idade? Clique abaixo e responda.",
+            "registerCityState" => "Digite abaixo a cidade onde você mora, e depois clique abaixo e selecione o estado onde você mora.",
+            "registerSex" => "Qual o seu gênero? Clique abaixo e responda.",
+            //"registerWork" => "Você está trabalhando atualmente?",
+            //"registerRent" => "Qual seu salário mensal?",
+            "registerScholarity" => "Qual o seu nível de escolaridade? Clique abaixo e responda.",
+            "registerFluency" => "Você é fluente em Libras? Clique abaixo e responda.",
+            "registerLibras" => "Desde qual idade você é sinalizante da Libras? Clique abaixo e responda. ",
+            "registerDeafness" => "Como você se identifica? Clique abaixo e responda.",
+            "registerParents" => "Você cresceu em uma casa com pais ou responsáveis sinalizantes da Libras? Clique abaixo e responda.",
+            "registerParentsDeafness" => "Seus pais ou responsáveis são surdos? Clique abaixo e responda.",
+            "registerQuestionnaire" => "Clique no botão abaixo para salvar todas as suas respostas.",
+
+            "susLibras1" => "Eu acho que gostaria de usar esse produto com frequência.",
+            "susLibras2" => "Eu acho o produto desnecessariamente complexo.",
+            "susLibras3" => "Eu achei o produto fácil de usar.",
+            "susLibras4" => "Eu acho que precisaria de ajuda de uma pessoa com conhecimentos técnicos para usar o produto.",
+            "susLibras5" => "Eu acho que as várias funções do produto estão muito bem integradas.",
+            "susLibras6" => "Eu acho que o produto apresenta muita inconsistência.",
+            "susLibras7" => "Eu acho que as pessoas aprenderão como usar esse produto rapidamente.",
+            "susLibras8" => "Eu achei o produto atrapalhado de usar.",
+            "susLibras9" => "Eu me senti confiante ao usar o produto.",
+            "susLibras10" => "Eu precisei aprender várias coisas novas antes de conseguir aprender a usar o produto.",
+
+            "susGlosa1" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) NOVAMENTE }3X USAR EU QUERER.",
+            "susGlosa2" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) ELE DIFÍIL, PRECISAR-NÃO, PODIA FÁCIL.",
+            "susGlosa3" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) ELE FÁCIL.",
+            "susGlosa4" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) EU (EXPRESSÃO DE DÚVIDA) / PROFISSIONAL ORIENTAR, PRECISAR.",
+            "susGlosa5" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) ELE ITENS CONECTADO, COMBINAR!",
+            "susGlosa6" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) ELE FALTAR.",
+            "susGlosa7" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) / ELE PESSOA MAIORIA POSSÍVEL APRENDER FÁCIL.",
+            "susGlosa8" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) ELE DIFÍCIL.",
+            "susGlosa9" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) BEM CONFIAR!",
+            "susGlosa10" => "Glosa: MATERIAIS VÁRIOS (PRODUTO) / DIFÍICIL TENTAR, ITENS APRENDER }2X, CONSEGUIR.",
 
 
-
-
-            "saveAge" => "Salvar",
+            //"saveAge" => "Salvar",
 
             //"newInterpreter" => "Novo Intérprete",
             "newInterpreter" => "Novo Usuário",
@@ -113,12 +137,43 @@
             "video-informe-data-inicio" => "/assets/video/14.mp4",
             "video-informe-data-fim" => "/assets/video/15.mp4",
             "video-selecione-interprete" => "/assets/video/16.mp4",
+            
+            "video-sociodemografico_idade" => "/assets/video/sd1n.mp4",
+            "video-sociodemografico_cidade_estado" => "/assets/video/sd2n.mp4",
+            "video-sociodemografico_genero" => "/assets/video/sd3n.mp4",
+            "video-sociodemografico_escolaridade" => "/assets/video/sd4n.mp4",
+            "video-sociodemografico_surdez" => "/assets/video/sd5n.mp4",
+            "video-sociodemografico_fluencia_libras" => "/assets/video/sd6n.mp4",
+            "video-sociodemografico_idade_libras" => "/assets/video/sd7n.mp4",
+            "video-sociodemografico_pais_libras" => "/assets/video/sd8n.mp4",
+            "video-sociodemografico_pais_surdos" => "/assets/video/sd9n.mp4",
+            "video-sociodemografico_salvar" => "/assets/video/sd10n.mp4",
+
+            "video-sus_pergunta1m" => "/assets/video/sus_p1m.mp4",
+            "video-sus_pergunta1" => "/assets/video/sus_p1.mp4",
+            "video-sus_pergunta2" => "/assets/video/sus_p2.mp4",
+            "video-sus_pergunta3" => "/assets/video/sus_p3.mp4",
+            "video-sus_pergunta4" => "/assets/video/sus_p4.mp4",
+            "video-sus_pergunta5" => "/assets/video/sus_p5.mp4",
+            "video-sus_pergunta6" => "/assets/video/sus_p6.mp4",
+            "video-sus_pergunta7" => "/assets/video/sus_p7.mp4",
+            "video-sus_pergunta8" => "/assets/video/sus_p8.mp4",
+            "video-sus_pergunta9" => "/assets/video/sus_p9.mp4",
+            "video-sus_pergunta10" => "/assets/video/sus_p10.mp4",
+
+            "video-sus_escala1" => "/assets/video/susEscala1.mp4",
+            "video-sus_escala2" => "/assets/video/susEscala2.mp4",
+            "video-sus_escala3" => "/assets/video/susEscala3.mp4",
+            "video-sus_escala4" => "/assets/video/susEscala4.mp4",
+            "video-sus_escala5" => "/assets/video/susEscala5.mp4",
+
+
 
             //"reportConfirmInterpreterLabel" => "Clique no botão abaixo para visualizar relatório de solicitação.",
             "reportConfirmInterpreterLabel" => "Clique no botão abaixo para visualizar relatório de solicitação de intérprete para evento.",
 
             //"reportConfirmInterpreter" => "Relatório de solicitação",
-            "reportConfirmInterpreter" => "Relatório de solicitação de intéprete",
+            "reportConfirmInterpreter" => "Relatório de solicitação de intérpretes",
 
             "reportConfirmedInterpreterLabel" => "Clique no botão abaixo para visualizar relatório de intérpretes confirmados em eventos.",
             "reportConfirmedInterpreter" => "Relatório de confirmação",
